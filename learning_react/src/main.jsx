b5ba6a77f3bd6c1139d0910bde7277dcb7f09609
NiloyBlueee <niloynilblue@gmail.com>
2025-06-16 02:19:16 +0600

    ez

--- learning_react/src/main.jsx
+++ learning_react/src/main.jsx
@@ -3,10 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import ColorPickerApp from './ColorPickerApp/ColorPickerApp.jsx'
 import App from './App.jsx'
+import ToDoApp from './To-Do List App/ToDoApp.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     {/*<ColorPickerApp />*/}
-    <App />
+    <ToDoApp />
 
   </StrictMode>,
 )
